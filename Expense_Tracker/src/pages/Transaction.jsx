160424e4c9f47fbdfd170658a01552e52615f208
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-07-11 23:26:45 +0530

    Add Transaction Page

---
+++ Expense_Tracker/src/pages/Transaction.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import AddTransaction from './AddTransaction'
+
+
+const Transaction = () => {
+  let storedTransactions= JSON.parse(localStorage.getItem("Transactions")) || [];
+  return (
+    <>
+        <AddTransaction />
+        <div className='w-full h-1.5 bg-gray-300'></div>
+
+        <h1 className='text-center text-3xl text-teal-400 m-3'>Transactions</h1>
+        <table className='table-auto border border-gray-400 w-full text-center'>
+          <thead>
+            <th>Ind</th>
+            <th className='border border-gray-400 px-4 py-2'>Categoory</th>
+            <th className='border border-gray-400 px-4 py-2'>Amount</th>
+            <th className='border border-gray-400 px-4 py-2'>Date</th>
+            <th className='border border-gray-400 px-4 py-2'>Description</th>
+            <th className='border border-gray-400 px-4 py-2'>Transaction Type</th>
+            
+          </thead>
+
+          <tbody>
+            
+              {
+              storedTransactions.map((val,ind)=>(
+                <tr key={ind} className='bg-gray-100'>
+                  <td className='border border-gray-400 px-4 py-2'>{ind}</td>
+                  <td className='border border-gray-400 px-4 py-2'>{val.category}</td>
+                  <td className={`border border-gray-400 px-4 py-2 ${val.transactionType==='expense'?'text-red-400':'text-green-400'}`}>{val.amount}</td>
+                  <td className='border border-gray-400 px-4 py-2'>{val.date}</td>
+                  <td className='border border-gray-400 px-4 py-2'>{val.description}</td>
+                  <td className='border border-gray-400 px-4 py-2'>{val.transactionType}</td>
+                </tr>
+                
+              ))
+              }
+            
+          </tbody>
+        </table>
+
+    </>
+  )
+}
+
+export default Transaction
