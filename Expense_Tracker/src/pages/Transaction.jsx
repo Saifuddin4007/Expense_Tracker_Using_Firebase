3ac874b1aaf9603a7a0fcaa98ef8e8782c5040e9
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-07-15 20:08:15 +0530

    Add handle update, handle delete and update button and remove <AddTransaction/> page and component from the Transaction Page

--- Expense_Tracker/src/pages/Transaction.jsx
+++ Expense_Tracker/src/pages/Transaction.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import AddTransaction from './AddTransaction'
-
+import { useState, useEffect } from 'react';
+import AddTransaction from './AddTransaction';
+import {useNavigate} from 'react-router-dom';
 
 const Transaction = () => {
-  let storedTransactions= JSON.parse(localStorage.getItem("Transactions")) || [];
+const navigate = useNavigate();
+const [transaction, setTransaction] = useState([])
+  
+
+  const handleEdit = (ind) =>{
+    const editTransac= transaction[ind];
+    navigate('/add-transaction', {state: {transaction: {...editTransac, ind}}})
+  }
+
+
+  const handleDelete = (ind) =>{
+    const updateTransac= transaction.filter((val,i)=>i!==ind);
+    setTransaction(updateTransac);
+    localStorage.setItem('Transactions', JSON.stringify(updateTransac));
+  }
+  useEffect(() => {
+    let storedTransactions= JSON.parse(localStorage.getItem("Transactions")) || [];
+    setTransaction(storedTransactions);
+    
+  }, [])
+  
+
+
   return (
     <>
-        <AddTransaction />
-        <div className='w-full h-1.5 bg-gray-300'></div>
+        {/* <AddTransaction /> */}
+        {/* <div className='w-full h-1.5 bg-gray-300'></div> */}
 
         <h1 className='text-center text-3xl text-teal-400 m-3'>Transactions</h1>
         <table className='table-auto border border-gray-400 w-full text-center'>
@@ -25,7 +48,7 @@ const Transaction = () => {
           <tbody>
             
               {
-              storedTransactions.map((val,ind)=>(
+              transaction.map((val,ind)=>(
                 <tr key={ind} className='bg-gray-100'>
                   <td className='border border-gray-400 px-4 py-2'>{ind}</td>
                   <td className='border border-gray-400 px-4 py-2'>{val.category}</td>
@@ -34,8 +57,8 @@ const Transaction = () => {
                   <td className='border border-gray-400 px-4 py-2'>{val.description}</td>
                   <td className='border border-gray-400 px-4 py-2'>{val.transactionType}</td>
                   <td className='border border-gray-400 px-4 py-2'>
-                    <button className='p-2.5 bg-green-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl'>Edit</button>
-                    <button className='p-2.5 bg-red-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl'>Delete</button>
+                    <button className='p-2.5 bg-green-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl cursor-pointer' onClick={()=>handleEdit(ind)}>Edit</button>
+                    <button className='p-2.5 bg-red-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl cursor-pointer' onClick={()=>handleDelete(ind)}>Delete</button>
                     </td>
                 </tr>
                 
