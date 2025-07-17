7139a8994e3b1917c6904fd78c0ba3829fbfd1bf
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-07-17 22:14:09 +0530

    Add NullTransaction page into the Transaction page

--- Expense_Tracker/src/pages/Transaction.jsx
+++ Expense_Tracker/src/pages/Transaction.jsx
@@ -1,72 +1,81 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import AddTransaction from './AddTransaction';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import NullTransactions from '../components/NullTransactions';
 
 const Transaction = () => {
-const navigate = useNavigate();
-const [transaction, setTransaction] = useState([])
-  
+  const navigate = useNavigate();
+  const [transaction, setTransaction] = useState([])
 
-  const handleEdit = (ind) =>{
-    const editTransac= transaction[ind];
-    navigate('/add-transaction', {state: {transaction: {...editTransac, ind}}})
+
+  const handleEdit = (ind) => {
+    const editTransac = transaction[ind];
+    navigate('/add-transaction', { state: { transaction: { ...editTransac, ind } } })
   }
 
 
-  const handleDelete = (ind) =>{
-    const updateTransac= transaction.filter((val,i)=>i!==ind);
+  const handleDelete = (ind) => {
+    const updateTransac = transaction.filter((val, i) => i !== ind);
     setTransaction(updateTransac);
     localStorage.setItem('Transactions', JSON.stringify(updateTransac));
   }
   useEffect(() => {
-    let storedTransactions= JSON.parse(localStorage.getItem("Transactions")) || [];
+    let storedTransactions = JSON.parse(localStorage.getItem("Transactions")) || [];
     setTransaction(storedTransactions);
-    
+
   }, [])
-  
+
 
 
   return (
     <>
-        {/* <AddTransaction /> */}
-        {/* <div className='w-full h-1.5 bg-gray-300'></div> */}
+      {/* <AddTransaction /> */}
+      {/* <div className='w-full h-1.5 bg-gray-300'></div> */}
 
-        <h1 className='text-center text-3xl text-teal-400 m-3'>Transactions</h1>
-        <table className='table-auto border border-gray-400 w-full text-center'>
-          <thead>
-            <th>Ind</th>
-            <th className='border border-gray-400 px-4 py-2'>Categoory</th>
-            <th className='border border-gray-400 px-4 py-2'>Amount</th>
-            <th className='border border-gray-400 px-4 py-2'>Date</th>
-            <th className='border border-gray-400 px-4 py-2'>Description</th>
-            <th className='border border-gray-400 px-4 py-2'>Transaction Type</th>
-            <th className='border border-gray-400 px-4 py-2'>Action</th>
-            
-          </thead>
+      {transaction.length === 0 ? ( <div className='flex justify-center items-center'>
+        <NullTransactions />
+      </div> ) : (
+        <>
 
-          <tbody>
-            
+          <h1 className='text-center text-3xl text-teal-400 m-3'>Transactions</h1>
+          <table className='table-auto border border-gray-400 w-full text-center'>
+            <thead>
+              <th>Ind</th>
+              <th className='border border-gray-400 px-4 py-2'>Categoory</th>
+              <th className='border border-gray-400 px-4 py-2'>Amount</th>
+              <th className='border border-gray-400 px-4 py-2'>Date</th>
+              <th className='border border-gray-400 px-4 py-2'>Description</th>
+              <th className='border border-gray-400 px-4 py-2'>Transaction Type</th>
+              <th className='border border-gray-400 px-4 py-2'>Action</th>
+
+            </thead>
+
+            <tbody>
+
               {
-              transaction.map((val,ind)=>(
-                <tr key={ind} className='bg-gray-100'>
-                  <td className='border border-gray-400 px-4 py-2'>{ind}</td>
-                  <td className='border border-gray-400 px-4 py-2'>{val.category}</td>
-                  <td className={`border border-gray-400 px-4 py-2 ${val.transactionType==='expense'?'text-red-400':'text-green-400'}`}>{val.amount}</td>
-                  <td className='border border-gray-400 px-4 py-2'>{val.date}</td>
-                  <td className='border border-gray-400 px-4 py-2'>{val.description}</td>
-                  <td className='border border-gray-400 px-4 py-2'>{val.transactionType}</td>
-                  <td className='border border-gray-400 px-4 py-2'>
-                    <button className='p-2.5 bg-green-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl cursor-pointer' onClick={()=>handleEdit(ind)}>Edit</button>
-                    <button className='p-2.5 bg-red-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl cursor-pointer' onClick={()=>handleDelete(ind)}>Delete</button>
+                transaction.map((val, ind) => (
+                  <tr key={ind} className='bg-gray-100'>
+                    <td className='border border-gray-400 px-4 py-2'>{ind}</td>
+                    <td className='border border-gray-400 px-4 py-2'>{val.category}</td>
+                    <td className={`border border-gray-400 px-4 py-2 ${val.transactionType === 'expense' ? 'text-red-400' : 'text-green-400'}`}>{val.amount}</td>
+                    <td className='border border-gray-400 px-4 py-2'>{val.date}</td>
+                    <td className='border border-gray-400 px-4 py-2'>{val.description}</td>
+                    <td className='border border-gray-400 px-4 py-2'>{val.transactionType}</td>
+                    <td className='border border-gray-400 px-4 py-2'>
+                      <button className='p-2.5 bg-green-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl cursor-pointer' onClick={() => handleEdit(ind)}>Edit</button>
+                      <button className='p-2.5 bg-red-300 border-0 outline-0 m-1.5 w-[100px] rounded-3xl cursor-pointer' onClick={() => handleDelete(ind)}>Delete</button>
                     </td>
-                </tr>
-                
-              ))
+                  </tr>
+
+                ))
               }
-            
-          </tbody>
-        </table>
+
+            </tbody>
+          </table>
+
+        </>
+      )}
 
     </>
   )
