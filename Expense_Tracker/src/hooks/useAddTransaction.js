b2b1a390e8e48f0b8e087ad80f97862e8a192f1d
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-07-20 13:57:03 +0530

    Update useAddTransaction

--- Expense_Tracker/src/hooks/useAddTransaction.js
+++ Expense_Tracker/src/hooks/useAddTransaction.js
@@ -1,21 +1,34 @@
-import {addDoc, collection, serverTimestamp} from 'firebase/firestore';
-import {db} from '../config/firebaseConfig';
-import {useGetUserInfo} from './useGetUserInfo';
+import {addDoc, collection, doc, updateDoc} from 'firebase/firestore';
+import {db} from '../config/FirebaseConfig';
+import {useGetUserInfo} from '../hooks/useGetUserInfo';
 
 export const useAddTransaction = () =>{
     const transactionCollectionRefer= collection(db, 'transactions');
-    const {userID} = useGetUserInfo();
-    const addTransaction = async ({description, transactionAmount, transactionType}) =>{
+    const {userId}= useGetUserInfo();
+    const addTransaction = async ({transactionType, amount, category, description, date}) =>{
         await addDoc(transactionCollectionRefer, {
-            userID,
-            description,
-            transactionAmount,
+            userId,
             transactionType,
-            createdAt: serverTimestamp()
+            amount,
+            category,
+            description,
+            date,
         });
-    }
+    };
 
-    console.log(userID);
+    const updateTransaction = async (id, { transactionType, amount, category, description, date }) => {
+        const transactionDoc = doc(db, 'transactions', id);
+        await updateDoc(transactionDoc, {
+            transactionType,
+            amount,
+            category,
+            description,
+            date,
+        });
+    };
 
-    return {addTransaction}
+    
+
+    return {addTransaction, updateTransaction}
 }
+
