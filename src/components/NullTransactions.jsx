f924b0c49397ca9315dac3a39d16e5c0e4cafe22
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-08-14 23:51:33 +0530

    Add responsive null transaction page

--- src/components/NullTransactions.jsx
+++ src/components/NullTransactions.jsx
@@ -3,12 +3,13 @@ import { PiInvoiceBold } from "react-icons/pi";
 
 const NullTransactions = () => {
   return (
-    <div className='w-[600px] m-5 bg-gray-600 p-3 rounded h-[350px] flex flex-col justify-center items-center'>
-        <PiInvoiceBold className='text-9xl text-center'/>
-        <h2 className='font-bold text-2xl p-3 m-3'>No Transactions Found</h2>
-          
+    <div className="w-full max-w-[600px] mx-auto m-3 sm:m-5 bg-gray-600 p-4 sm:p-3 rounded h-[250px] sm:h-[350px] flex flex-col justify-center items-center">
+      <PiInvoiceBold className="text-6xl sm:text-9xl text-center" />
+      <h2 className="font-bold text-lg sm:text-2xl p-2 sm:p-3 m-2 sm:m-3 text-center">
+        No Transactions Found
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default NullTransactions
+export default NullTransactions;
