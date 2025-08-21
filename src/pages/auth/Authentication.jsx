e5e91b090befcb8bf13d5d2fba6668f45385b8c3
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2025-08-21 09:00:20 +0530

    Update authentication page and make it especially for google auth

--- src/pages/auth/Authentication.jsx
+++ src/pages/auth/Authentication.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Authentication = () => {
     const navigate= useNavigate();
-    const handleSignIn = async () =>{
+    const handleSignInWithGoogle = async () =>{
         try {
             const result = await signInWithPopup(auth, provider);
             console.log(result);
@@ -23,18 +23,26 @@ const Authentication = () => {
         
     }
 
+    const handleEmailBtn = () =>{
+        navigate('/email-auth');
+    }
+
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-blue-500 to-violet-600">
-            <div className="bg-white p-10 rounded-2xl shadow-xl text-center">
+            <div className="flex flex-col gap-2 bg-white p-10 rounded-2xl shadow-xl text-center">
                 <h2 className="text-2xl font-bold text-gray-800 mb-4">Sign In</h2>
                 <p className="text-gray-500 mb-6">Sign in with your Google account to continue</p>
                 <button
-                    onClick={handleSignIn}
+                    onClick={handleSignInWithGoogle}
                     className="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-4 rounded-xl transition"
                 >
                     Sign In With Google
                 </button>
+                <button 
+                onClick={handleEmailBtn}
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-4 rounded-xl transition"
+                >Email Auth</button>
             </div>
         </div>
   )
